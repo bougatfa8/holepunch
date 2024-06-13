0afb0809dc85b734135b94f77efb4b16ba231ff7
Ahmed Bougatf <98043267+bougatfa8@users.noreply.github.com>
2024-06-13 17:39:58 +0400

    Update test.js

--- test.js
+++ test.js
@@ -8,160 +8,202 @@ let room_core = new Hypercore('room');
 const swarm = new Hyperswarm()
 const conns = []
 let core
-let topic
-
+let topic 
 document.querySelector('#create-chat-room').addEventListener('click', formApply);
 document.querySelector('#join-chat-room').addEventListener('click', formApplyTopic);
 
+// document.querySelector('#messages').addEventListener('submit', sendMessage)
+    
+
+
+
 async function initializeRoom() {
-  await room_core.ready();
+  await room_core.ready();  // Wait for the room_core to be ready
   const chatRoomTopic = document.querySelector('#chat-room-topic');
   console.log('Initializing room');
 
-  if (room_core.length > 0) {
-    for (let i = 0; i < room_core.length; i++) {
+  if(room_core.length  > 0) {
+
+  
+  for (let i = 0; i < room_core.length; i++) {
       const block = await room_core.get(i);
       const jsonObject = JSON.parse(block);
+      // Retrieve the key from the object
       const key = Object.keys(jsonObject)[0];
       const value = jsonObject[key];
-      console.log(value);
+      console.log(value)
       const newDiv = document.createElement('div');
-      newDiv.className = 'chat-room-topic';
+      newDiv.className = 'chat-room-topic'; 
       newDiv.innerText = key;
       newDiv.addEventListener('click', () => {
         document.querySelector('#messages').innerHTML = '';
-        retrieveMessage(value);
+        retrieveMessage(value)
         topic = value;
-      });
+    });
+  
       chatRoomTopic.appendChild(newDiv);
-    }
+  }
+  await room_core.close()
   }
 }
 
+
+// Call the function to initialize the room
 initializeRoom();
 
+
 swarm.on('connection', conn => {
-  const name = b4a.toString(conn.remotePublicKey, 'hex');
-  console.log('* got a connection from:', name, '*');
-  conns.push(conn);
-  conn.once('close', () => conns.splice(conns.indexOf(conn), 1));
-  conn.on('data', async data => {
-    console.log(`${name}: ${data}`);
-    const roomMessage = JSON.stringify({ [name]: data });
-    if (core && core.writable) {
-      await core.append(roomMessage);
-    }
-  });
-  conn.on('error', e => console.log(`Connection error: ${e}`));
-});
+    const name = b4a.toString(conn.remotePublicKey, 'hex')
+    console.log('* got a connection from:', name, '*')
+    conns.push(conn)
+    conn.once('close', () => conns.splice(conns.indexOf(conn), 1))
+    conn.on('data', data => {
+      console.log(`${name}: ${data}`)
+      const roomMessage = JSON.stringify({ [name]: data });
+      // Append the JSON string to room_core
+      core.append(roomMessage);
 
-function formApply() {
-  const roomName = document.querySelector('#NameOrTopicInput').value;
+      onMessageAdded (name, data) 
+    })
+    conn.on('error', e => console.log(`Connection error: ${e}`))
+  })
+
+
+function formApply(){
+  var roomName =document.querySelector('#NameOrTopicInput').value;
   if (roomName) {
-    createChatRoom(roomName);
+      createChatRoom(roomName)
   } else {
-    alert('Room creation cancelled.');
+      alert('Room creation cancelled.');
   }
 }
 
-function formApplyTopic() {
-  const topic = document.querySelector('#NameOrTopicInput').value;
+
+function formApplyTopic(){
+  var topic =document.querySelector('#NameOrTopicInput').value;
   if (topic) {
-    joinSwarmTopic(topic);
+    joinSwarmTopic(topic)
   } else {
-    alert('Room Join cancelled.');
+      alert('Room Joined cancelled.');
   }
 }
+  
+
+
 
 function createChatRoom(roomName) {
-  const topicBuffer = crypto.randomBytes(32);
-  joinSwarm(topicBuffer, roomName);
+  const topicBuffer = crypto.randomBytes(32)
+  joinSwarm(topicBuffer,roomName)
 }
 
-async function joinSwarm(topicBuffer, roomName) {
-  const discovery = swarm.join(topicBuffer, { client: true, server: true });
-  const topic = b4a.toString(topicBuffer, 'hex');
-  alert('Creating room: ' + roomName + ' with topic: ' + topic);
+async function joinSwarm (topicBuffer,roomName) {
+    const discovery = swarm.join(topicBuffer, { client: true, server: true })  
+    const topic = b4a.toString(topicBuffer, 'hex')
+    alert('Creating room: ' + roomName + ' with topic: ' + topic);
+    // Convert the object to a JSON string
+    const roomData = JSON.stringify({ [roomName]: topic });
 
-  const roomData = JSON.stringify({ [roomName]: topic });
-  await room_core.append(roomData);
-  const chatRoomTopic = document.querySelector('#chat-room-topic');
+    // Append the JSON string to room_core
+    room_core.append(roomData);
+    const chatRoomTopic = document.querySelector('#chat-room-topic');
 
-  const newDiv = document.createElement('div');
-  newDiv.className = 'chat-room-topic';
-  newDiv.innerText = roomName;
-  newDiv.addEventListener('click', () => {
-    document.querySelector('#messages').innerHTML = '';
-    retrieveMessage(topic);
-  });
-  chatRoomTopic.appendChild(newDiv);
+    const newDiv = document.createElement('div');
+    newDiv.className = 'chat-room-topic'; // Assign the class to the new div
+    newDiv.innerText = roomName;
+    await room.close()
 
-  core = new Hypercore(topic);
-  await core.ready();
-  discovery.flushed();
-}
+    newDiv.addEventListener('click', () => {
+      document.querySelector('#messages').innerHTML = '';
 
-async function joinSwarmTopic(topicString) {
-  const topicBuffer = b4a.from(topicString, 'hex');
-  console.log(topicBuffer);
-  const discovery = swarm.join(topicBuffer, { client: true, server: true });
-  const topic = topicString;
-  alert('Joining room with topic: ' + topic);
+      retrieveMessage(topic)
+  })
+  ;
+    await room_core.close()
 
-  const chatRoomTopic = document.querySelector('#chat-room-topic');
-  const newDiv = document.createElement('div');
-  newDiv.className = 'chat-room-topic';
-  newDiv.innerText = 'Joined room';
-
-  newDiv.addEventListener('click', () => {
-    document.querySelector('#messages').innerHTML = '';
-    retrieveMessage(topic);
-  });
-  chatRoomTopic.appendChild(newDiv);
-
-  core = new Hypercore(topic);
-  await core.ready();
-  discovery.flushed();
-}
-
-document.querySelector('#message-form').addEventListener('submit', sendMessage);
-
-async function retrieveMessage(topic) {
-  console.log('retrieve message');
-  core = new Hypercore(topic);
-  await core.ready();
-  for (let i = 0; i < core.length; i++) {
-    const block = await core.get(i);
-    const jsonObject = JSON.parse(block);
-    const from = Object.keys(jsonObject)[0];
-    const message = jsonObject[from];
-    console.log(message);
-
-    const $div = document.createElement('div');
-    $div.textContent = `<${from}> ${message}`;
-    document.querySelector('#messages').appendChild($div);
-  }
-}
-
-async function sendMessage(e) {
-  e.preventDefault();
-  const message = document.querySelector('#message').value;
-  document.querySelector('#message').value = '';
-  onMessageAdded('You', message);
-  const roomMessage = JSON.stringify({ You: message });
-
-  if (core && core.writable) {
-    await core.append(roomMessage);
+    chatRoomTopic.appendChild(newDiv);
+    core = new Hypercore(topic)
+    discovery.flushed()
   }
 
-  for (const peer of conns) {
-    peer.write(message);
-  }
-  console.log('Sent message');
-}
 
-function onMessageAdded(from, message) {
-  const $div = document.createElement('div');
-  $div.textContent = `<${from}> ${message}`;
-  document.querySelector('#messages').appendChild($div);
-}
+
+  async function joinSwarmTopic (topicString) {
+    const topicBuffer = b4a.from(topicString,'hex')
+    console.log(topicBuffer)
+    const discovery = swarm.join(topicBuffer, { client: true, server: true })  
+    const topic =topicString
+    alert('Creating room: ' + 'roomName' + ' with topic: ' + topic);
+    // Convert the object to a JSON string
+    const roomData = JSON.stringify({ 'roomName': topic });
+
+    // Append the JSON string to room_core
+    room_core.append(roomData);
+    const chatRoomTopic = document.querySelector('#chat-room-topic');
+
+    const newDiv = document.createElement('div');
+    newDiv.className = 'chat-room-topic'; // Assign the class to the new div
+    newDiv.innerText = 'roomName';
+
+    newDiv.addEventListener('click', () => {
+      document.querySelector('#messages').innerHTML = '';
+
+      retrieveMessage(topic)
+  })
+  ;
+    await room_core.close()
+
+    chatRoomTopic.appendChild(newDiv);
+    core = new Hypercore(topic)
+    discovery.flushed()
+  }
+
+
+
+  document.querySelector('#message-form').addEventListener('submit', sendMessage)
+
+  async function retrieveMessage(topic) {
+
+    console.log('retrieve message');
+    core = new Hypercore(topic);
+  
+    await core.ready();
+    for (let i = 0; i < core.length; i++) {
+      const block = await core.get(i); // Await the promise resolution
+      const jsonObject = JSON.parse(block);
+      
+      // Retrieve the key from the object
+      const from = Object.keys(jsonObject)[0];
+      const message = jsonObject[from];
+      console.log(message); // Log the message
+  
+      const $div = document.createElement('div');
+      $div.textContent = `<${from}> ${message}`; // Correctly set the text content
+      document.querySelector('#messages').appendChild($div);
+    }
+    core.close();
+  }
+  
+
+  function sendMessage (e) {
+    const core = new Hypercore(topic);
+    const message = document.querySelector('#message').value
+    document.querySelector('#message').value = ''
+    e.preventDefault()
+    onMessageAdded('You', message)
+    // Send the message to all peers (that you are connected to)
+    const peers = [...swarm.connections]
+      for (const peer of peers) {
+        peer.write(message)
+      }
+      console.log('Sent message')
+      const roomMessage = JSON.stringify({ You: message });
+      // Append the JSON string to room_core
+      core.append(roomMessage);
+  }
+  
+  // appends element to #messages element with content set to sender and message
+  function onMessageAdded (from, message) {
+    const $div = document.createElement('div')
+    $div.textContent = `<${from}> ${message}`
+    document.querySelector('#messages').appendChild($div)
+  }
